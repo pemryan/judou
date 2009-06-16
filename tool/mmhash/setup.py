2031532643c9229f78a0f46849a5da33188593d7
zsp007 <zsp007@2b19d78c-24d0-11de-82e8-8d21a8f7b98a>
2009-06-16 04:11:19 +0000

    mmhash

--- tool/mmhash/setup.py
+++ tool/mmhash/setup.py
@@ -2,11 +2,18 @@ from distutils.core import setup, Extension
 
 setup(
         name = 'mmhash',
-        version = '1.1',
+        version = '1.2',
         ext_modules = [
             Extension('mmhash', ['mmhash.cpp'],
                 extra_compile_args=['-O3', '-pipe',
                 '-fomit-frame-pointer']),
             ],
         description="MurmurHash2 http://murmurhash.googlepages.com/",
+        long_description="""
+import mmhash
+print mmhash.get_hash(str(range(1000)))
+-1624558063
+print mmhash.get_unsigned_hash(str(range(1000)))
+2670409233
+        """
 )
