637367e0d8433dbfdbdb08940b4ae68b9601aecf
zsp007 <zsp007@2b19d78c-24d0-11de-82e8-8d21a8f7b98a>
2009-06-16 03:42:19 +0000

    fix bug

--- tool/mmhash/setup.py
+++ tool/mmhash/setup.py
@@ -2,10 +2,11 @@ from distutils.core import setup, Extension
 
 setup(
         name = 'mmhash',
-        version = '1.0',
+        version = '1.1',
         ext_modules = [
             Extension('mmhash', ['mmhash.cpp'],
                 extra_compile_args=['-O3', '-pipe',
                 '-fomit-frame-pointer']),
             ],
+        description="MurmurHash2 http://murmurhash.googlepages.com/",
 )
