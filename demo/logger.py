d12c18b1819f621fbcc1ebd6a5356f26838f190d
twinsant <twinsant@2b19d78c-24d0-11de-82e8-8d21a8f7b98a>
2009-06-18 09:54:54 +0000

    Add: judou dict builder

---
+++ demo/logger.py
@@ -0,0 +1,33 @@
+# @desc: Logging wrapper
+import sys
+import logging
+from datetime import datetime
+
+level = None
+
+def init():
+    global level
+
+    if len(sys.argv) > 1 and sys.argv[1]=='debug':
+        level = logging.DEBUG
+    else:
+        level = logging.INFO
+    logging.basicConfig(level=level,
+                       format='%(asctime)s %(levelname)s %(message)s')
+
+def is_debug():
+    return logging.DEBUG == level
+
+debug=logging.debug
+info=logging.info
+warning=logging.warning
+exception=logging.exception
+error=logging.error
+
+class Timer(object):
+    def start(self):
+        self.start = datetime.now()
+
+    def end(self):
+        elapsed = datetime.now() - self.start
+        return elapsed
